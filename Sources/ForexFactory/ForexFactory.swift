586104cf6d005c43e3fbbf14d1e38c94b0a0c91f
Szymon <contact@szymonlorenz.com>
2025-03-18 19:41:23 +1000

    Fix forex API

--- Sources/ForexFactory/ForexFactory.swift
+++ Sources/ForexFactory/ForexFactory.swift
@@ -1,20 +1,28 @@
 import Foundation
 
-public enum Impact: String, Decodable {
+public enum Impact: String, Codable {
     case high = "High"
     case medium = "Medium"
     case low = "Low"
+    case other
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        let rawValue = try container.decode(String.self)
+
+        self = Impact(rawValue: rawValue) ?? .other
+    }
 }
 
-public struct ForexEvent: Decodable {
+public struct ForexEvent: Codable {
+    public let id: UUID = UUID()
     public let title: String
     public let country: String
-    public let date: String
+    public let date: Date
     public let impact: Impact
     public let forecast: String?
     public let previous: String?
     public let actual: String?
-    public let timestamp: Int
 }
 
 public struct ForexCalendar: Decodable {
@@ -24,11 +32,11 @@ public struct ForexCalendar: Decodable {
 public extension [ForexEvent] {
     func eventsForToday() -> [ForexEvent] {
         let today = Date()
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd"
-        let todayString = dateFormatter.string(from: today)
+        let calendar = Calendar.current
         
-        return self.filter { $0.date.contains(todayString) }
+        return self.filter { event in
+            calendar.isDate(event.date, inSameDayAs: today)
+        }
     }
     
     func events(withImpact impact: Impact = .high) -> [ForexEvent] {
@@ -42,11 +50,85 @@ public extension [ForexEvent] {
 }
 
 public final class ForexAPI {
-    public let baseUrl = URL(string: "https://nfs.faireconomy.media/ff_calendar_thisweek.json")!
+    public enum Error: Swift.Error {
+        case wrongResponseType
+        case failed(status: Int, message: String)
+    }
+    private static let baseUrl = URL(string: "https://nfs.faireconomy.media/ff_calendar_thisweek.json")!
 
-    public func fetchEvents() async throws -> [ForexEvent] {
-        let (data, _) = try await URLSession.shared.data(from: baseUrl)
-        let calendar = try JSONDecoder().decode(ForexCalendar.self, from: data)
-        return calendar.events
+    public static func fetchEvents() async throws -> [ForexEvent] {
+        let (data, response) = try await URLSession.shared.data(from: baseUrl)
+        guard let httpResponse = response as? HTTPURLResponse else {
+            throw Error.wrongResponseType
+        }
+        guard 200..<300 ~= httpResponse.statusCode else {
+            throw Error.failed(status: httpResponse.statusCode, message: String(data: data, encoding: .utf8) ?? "")
+        }
+        let events = try jsonDecoder.decode([ForexEvent].self, from: data)
+        return events
     }
 }
+
+
+// MARK: - Helpers
+
+private let customDateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.calendar = Calendar(identifier: Calendar.Identifier.iso8601)
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.dateFormat = "h:mm MM/dd/yy"
+    return formatter
+}()
+
+
+private let iso8601DateFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.calendar = Calendar(identifier: Calendar.Identifier.iso8601)
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+    return formatter
+}()
+
+private let iso8601DateOnlyFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.calendar = Calendar(identifier: Calendar.Identifier.iso8601)
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.dateFormat = "yyyy-MM-dd"
+    return formatter
+}()
+
+private let iso8601DateMillisecondsFormatter: DateFormatter = {
+    let formatter = DateFormatter()
+    formatter.calendar = Calendar(identifier: Calendar.Identifier.iso8601)
+    formatter.locale = Locale(identifier: "en_US_POSIX")
+    formatter.timeZone = TimeZone(secondsFromGMT: 0)
+    formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZZZZZ"
+    return formatter
+}()
+
+private let jsonDecoder: JSONDecoder = {
+    let decoder = JSONDecoder()
+    decoder.nonConformingFloatDecodingStrategy = .convertFromString(
+        positiveInfinity: "Infinity",
+        negativeInfinity: "-Infinity",
+        nan: "NaN"
+    )
+    decoder.dateDecodingStrategy = .custom({ decoder in
+        let container = try decoder.singleValueContainer()
+        let value = try container.decode(String.self)
+        if let date = iso8601DateFormatter.date(from: value) {
+            return date
+        }
+        if let date = iso8601DateMillisecondsFormatter.date(from: value) {
+            return date
+        }
+        if let date = iso8601DateOnlyFormatter.date(from: value) {
+            return date
+        }
+        throw DecodingError.dataCorruptedError(in: container, debugDescription: "Invalid date format: \(value)")
+    })
+    return decoder
+}()
